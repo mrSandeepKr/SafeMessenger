dd25b9d8e5d57eec139893b7cab96fc53c3f51f6
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2021-05-21 01:27:44 +0530

    init

---
+++ SafeMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeMessenger
+//
+//  Created by Sandeep Kumar on 21/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
